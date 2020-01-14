2e0942b43a4b5b4cdaaa849fc1f2e4f66a3d9084
Lewis Walwyn <lewiswalwyn@Lewiss-MacBook-Pro.local>
2020-01-14 13:03:01 +0000

    installed cors

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/apiRouter")
 const { psqlErrorHandler, baseErrorHandler, miscErrorHandler, methodErrorHandler } = require("./errorHandler")
+const cors = require('cors');
+
+app.use(cors());
+
 
 app.use(express.json());
 
